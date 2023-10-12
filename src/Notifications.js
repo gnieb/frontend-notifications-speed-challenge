3cca335311ad25063143d0dca29102d697c93581
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-12 17:37:01 -0400

    mark all as read completed

--- src/Notifications.js
+++ src/Notifications.js
@@ -3,7 +3,7 @@ import Notification from './Notification'
 
 
 export default function Notifications () {
-    const [notifs, setNotifs] = useState([{ from: "Noah", notification: "sent you a private message", isRead: false}, {from: "Mom", notification:"commented on a post you're tagged in", isRead: false}, { from: "Jack", notification: "sent you a private message", isRead: false}, { from: "Boss", notification: "sent you a private message", isRead: true}])
+    const [notifs, setNotifs] = useState([{ from: "Noah", notification: "sent you a private message", isRead: false,}, {from: "Mom", notification:"commented on a post you're tagged in", isRead: false}, { from: "Jack", notification: "sent you a private message", isRead: false}, { from: "Boss", notification: "sent you a private message", isRead: true}])
     
     const filterByisRead = (notif) => (notif.isRead == false)
     const filteredByUnread = notifs.filter(filterByisRead)
@@ -12,13 +12,32 @@ export default function Notifications () {
         <Notification key={i} n={n} />
     ))
     
+    const handleAllRead = () => {
+       
+      const read =  notifs.map(n => {
+        console.log(n)
+        return n = {
+        ...n, 
+        isRead:true
+        }
+      });
+    
+   
+     setNotifs(read)
+    console.log("YAY")
+    }
     
     return (
         <div>
-            <div className="font-bold p-3 flex">
-                <div className="text-[#1c202b]">Notifications</div>
-                <div className="mx-1 bg-[#5e6778] text-white w-10 rounded-full text-center">
-                {filteredByUnread.length}
+            <div className="font-bold p-3 flex justify-between">
+                <div className="flex">
+                    <div className="text-[#1c202b]">Notifications</div>
+                    <div className="mx-1 bg-[#5e6778] text-white w-10 rounded-full text-center">
+                    {filteredByUnread.length}
+                    </div>
+                </div>
+                <div>
+                    <button onClick={handleAllRead}>Mark All as Read</button>
                 </div>
             </div>
            <div>{displayNotifs}</div>
